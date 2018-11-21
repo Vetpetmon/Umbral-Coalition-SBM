322c8f4b68a1b31f1ec820706abc3501cc9e8af4
Vetpetmon-Labs <vetpetmon@hotmail.com>
2018-11-20 23:40:02 -0500

    Welcome to Honex,

---
+++ items/active/weapons/ranged/abilities/spraycan/mustard/mustardspray.lua
@@ -0,0 +1,36 @@
+require "/items/active/weapons/ranged/gunfire.lua"
+
+FlamethrowerAttack = GunFire:new()
+
+function UCSprayCanMustardAttack:init()
+  GunFire.init(self)
+
+  self.active = false
+end
+
+function UCSprayCanMustardAttack:update(dt, fireMode, shiftHeld)
+  GunFire.update(self, dt, fireMode, shiftHeld)
+
+  if self.weapon.currentAbility == self then
+    if not self.active then self:activate() end
+  elseif self.active then
+    self:deactivate()
+  end
+end
+
+function UCSprayCanMustardAttack:muzzleFlash()
+  --null muzzle flash
+end
+
+function UCSprayCanMustardAttack:activate()
+  self.active = true
+  animator.playSound("fireStart")
+  animator.playSound("fireLoop", -1)
+end
+
+function UCSprayCanMustardAttack:deactivate()
+  self.active = false
+  animator.stopAllSounds("fireStart") --*shk shk shk*
+  animator.stopAllSounds("fireLoop") --spray
+  animator.playSound("fireEnd") --end spray
+end
