bafe4d18530b7702cc4d98b96ff96afe8dab9856
Vetpetmon-Labs <vetpetmon@hotmail.com>
2019-01-21 15:15:35 -0500

    imagine stepping on SCP-387
    this is getting to be a MESS

--- objects/ucmissionteleporters/limbo/limboaccessteleporter.lua
+++ objects/ucmissionteleporters/limbo/limboaccessteleporter.lua
@@ -5,55 +5,81 @@ function init()
   self.detectArea[1] = object.toAbsolutePosition(self.detectArea[1])
   self.detectArea[2] = object.toAbsolutePosition(self.detectArea[2])
 
-  storage.uuid = storage.uuid or sb.makeUuid()
-  object.setInteractive(true)
+  storage.active = storage.active or config.getParameter("startActive", false)
 
--- Do not want. causes vanishing
---  message.setHandler("onTeleport", function(message, isLocal, data)
---      if not storage.vanishTime then
---        storage.vanishTime = world.time() + config.getParameter("vanishTime")
---      end
---    end)
+  message.setHandler("activate", function()
+    storage.active = true
+    animator.setAnimationState("teleporter", "on")
+    object.setLightColor(config.getParameter("lightColor", {50, 255, 50}))
+  end)
 
+  message.setHandler("isActive", function()
+    return storage.active == true
+  end)
+
+  self.limboActiveTime = config.getParameter("limboActiveTime", 60)
+  self.limboInstanceOptions = config.getParameter("limboInstance")
+  self.limboAccessConfig = root.assetJson("/interface/scripted/limboteleporter/limboteleportergui.config")
+
+  storage.limboActive = storage.limboActive or false
+
+  message.setHandler("activateLimbo", function()
+    if not storage.limboActive then
+      storage.limboActive = true
+      storage.limboCloseTime = world.time() + self.limboActiveTime
+      math.randomseed(util.seedTime())
+      local instanceOption = util.randomFromList(self.limboInstance)
+      storage.limboType = instanceOption[1]
+      storage.limboWorldId = string.format("InstanceWorld:%s:%s:%s", instanceOption[2], sb.makeUuid(), 8)
+
+      animator.setGlobalTag("destination", storage.limboType)
+      animator.setAnimationState("teleporter", "on")
+      animator.playSound("on");
+      object.setLightColor(config.getParameter("lightColor", {50, 255, 50}))
+    end
+  end)
+
+  message.setHandler("closelimbo", function()
+    if storage.limboActive then
+      closelimbo()
+    end
+  end)
+
+  if storage.active or storage.limboActive then
+    animator.setAnimationState("console", "on")
+    object.setLightColor(config.getParameter("lightColor", {255, 255, 255}))
+  else
+    animator.setAnimationState("console", "off")
+    object.setLightColor({0, 0, 0, 0})
+  end
+
+  if storage.limboActive then
+    animator.setGlobalTag("destination", storage.limboType)
+    animator.setAnimationState("portal", "openloop")
+  end
+end
+
+function onInteraction()
+    if not storage.limboActive then
+      return { "ScriptPane", "/interface/scripted/limboconsole/limboconsolegui.config" }
+    else
+      self.limboAccessConfig.closeTime = storage.limboCloseTime
+      self.limboAccessConfig.activeTime = self.limboActiveTime
+      self.limboAccessConfig.worldId = storage.limboWorldId
+      return { "ScriptPane", self.limboAccessConfig }
+    end
 end
 
 function update(dt)
-
-  local players = world.entityQuery(self.detectArea[1], self.detectArea[2], {
-      includedTypes = {"player"},
-      boundMode = "CollisionArea"
-    })
-
---if storage.vanishTime and world.time() > storage.vanishTime then
---    object.smash()
---  end
-
+  if storage.limboActive then
+    if world.time() > storage.limboCloseTime then
+      closelimbo()
+    end
+  end
 end
 
-function onInteraction(args)
-  if config.getParameter("returnDoor") then
-    return { "OpenTeleportDialog", {
-        canBookmark = false,
-        includePlayerBookmarks = false,
-        destinations = { {
-          name = "Door",
-          planetName = "Exit",
-          icon = "return",
-          warpAction = "Return"
-        } }
-      }
-    }
-  else
-    return { "OpenTeleportDialog", {
-        canBookmark = false,
-        includePlayerBookmarks = false,
-        destinations = { {
-          name = "Door",
-          planetName = "Enter",
-          icon = "default",
-          warpAction = string.format(config.getParameter("destination"), storage.uuid, world.threatLevel())
-        } }
-      }
-    }
-  end
+function closelimbo()
+  storage.limboActive = false
+  animator.setAnimationState("console", "off")
+  object.setLightColor({0, 0, 0, 0})
 end
