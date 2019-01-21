762c7d09dd144ab62d7b95006194c2a3404c3e02
Vetpetmon-Labs <vetpetmon@hotmail.com>
2019-01-21 00:53:01 -0500

    boof

---
+++ objects/ucmissionteleporters/limbo/limboaccessteleporter.lua
@@ -0,0 +1,59 @@
+require "/scripts/util.lua"
+
+function init()
+  self.detectArea = config.getParameter("detectArea")
+  self.detectArea[1] = object.toAbsolutePosition(self.detectArea[1])
+  self.detectArea[2] = object.toAbsolutePosition(self.detectArea[2])
+
+  storage.uuid = storage.uuid or sb.makeUuid()
+  object.setInteractive(true)
+
+-- Do not want. causes vanishing
+--  message.setHandler("onTeleport", function(message, isLocal, data)
+--      if not storage.vanishTime then
+--        storage.vanishTime = world.time() + config.getParameter("vanishTime")
+--      end
+--    end)
+
+end
+
+function update(dt)
+
+  local players = world.entityQuery(self.detectArea[1], self.detectArea[2], {
+      includedTypes = {"player"},
+      boundMode = "CollisionArea"
+    })
+
+--if storage.vanishTime and world.time() > storage.vanishTime then
+--    object.smash()
+--  end
+
+end
+
+function onInteraction(args)
+  if config.getParameter("returnDoor") then
+    return { "OpenTeleportDialog", {
+        canBookmark = false,
+        includePlayerBookmarks = false,
+        destinations = { {
+          name = "Door",
+          planetName = "Exit",
+          icon = "return",
+          warpAction = "Return"
+        } }
+      }
+    }
+  else
+    return { "OpenTeleportDialog", {
+        canBookmark = false,
+        includePlayerBookmarks = false,
+        destinations = { {
+          name = "Door",
+          planetName = "Enter",
+          icon = "default",
+          warpAction = string.format(config.getParameter("destination"), storage.uuid, world.threatLevel())
+        } }
+      }
+    }
+  end
+end
