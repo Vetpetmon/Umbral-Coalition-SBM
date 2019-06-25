ec25e3c4798dd7ee302e29b627a62a8a6f22ae4d
Veptetmon <vetpetmon@gmail.com>
2019-06-25 12:24:21 -0400

    Make Obliterator hurt

---
+++ stats/effects/combat/weapon/obliteratorbeam.lua
@@ -0,0 +1,26 @@
+
+function init()
+  effect.setParentDirectives("fade=FF0000=0.70")
+  script.setUpdateDelta(3)
+
+  self.tickDamagePercentage = 0.005
+  self.tickTime = 0.1
+  self.tickTimer = self.tickTime
+end
+
+function update(dt)
+  self.tickTimer = self.tickTimer - dt
+    if self.tickTimer <= 0 then
+      self.tickTimer = self.tickTime
+      status.applySelfDamageRequest({
+        damageType = "IgnoresDef",
+        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) * 2,
+        damageSourceKind = "fire",
+        sourceEntityId = entity.id()
+      })
+  end
+end
+
+function uninit()
+
+end
---
+++ stats/effects/combat/weapon/obliteratorheld.lua
@@ -0,0 +1,30 @@
+
+function init()
+  script.setUpdateDelta(5)
+
+  self.tickDamagePercentage = 0.005
+  self.tickTime = 0.2
+  self.tickTimer = self.tickTime
+  self.effectHandler=effect.addStatModifierGroup({{stat = "foodDelta", baseMultiplier = 150.0}}) --120x the hunger drain.
+  status.removeEphemeralEffect("wellfed",math.huge)
+end
+
+function update(dt)
+  self.tickTimer = self.tickTimer - dt
+    if self.tickTimer <= 0 then
+      self.tickTimer = self.tickTime
+      status.applySelfDamageRequest({
+        damageType = "IgnoresDef",
+        damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) * 3,
+        damageSourceKind = "fire",
+        sourceEntityId = entity.id()
+      })
+  end
+  mcontroller.controlModifiers({
+      speedModifier = 0.9,
+    })
+end
+
+function uninit()
+
+end
