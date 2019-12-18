6558444aadcdfe26ee629721bbedcff407f3396e
Veptetmon <vetpetmon@gmail.com>
2019-12-17 21:09:32 -0500

    QUOTE SNAPPED IN KFAD Y'ALL anyways, Xenonian stats are now in-mod

---
+++ stats/effects/racial/xenonian.lua
@@ -0,0 +1,18 @@
+function init()
+  --These lines will eliminate the need to use FR for certain racial bonuses, allowing for in-mod bonuses, but with optional FR goodies.
+  effect.addStatModifierGroup({
+    {stat = "slimeImmunity", amount = 1},
+    {stat = "slimestickImmunity", amount = 1},
+    {stat = "poisonStatusImmunity", amount = 1},
+    {stat = "xenonian", amount = 1},
+    {stat = "baseDiseaseImmunity", amount = 1},
+    {stat = "poisonResistance", amount = 0.4},
+    {stat = "electricResistance", amount = 0.3},
+    {stat = "cosmicResistance", amount = -0.3}
+  })
+end
+function update(dt)
+end
+
+function uninit()
+end
